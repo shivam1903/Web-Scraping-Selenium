4bdf54847563b38e83347246850722c78cbaf67c
shivam1903 <shivam234agarwal@gmail.com>
2024-02-18 02:14:51 +0530

    Adding the final files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Web-Scraping-Selenium
 This repo contains my practice code for working with Selenium to implement Web Scraping. Also involves having Captcha identification
+
+repo link: https://github.com/shivam1903/Web-Scraping-Selenium
+
+For the mentioned tasks, I have used chrome webdriver.
+
+## Step 1 - Company Name
+I start by entering the company name in the search box. I identify the search box using its ID.
+Once the name is entered, we move to the captcha.
+
+## Step 2 - Captcha
+Captcha was the tricky part. For this part I tried user pytesseract initially, and then I used easyOCR. Both of them did not give perfect results. I realized OCR will not always give perfect answers for Captcha. I could have created a ML model to deal with this. But for simplicity, I initiated a for loop which keeps checking the Captcha, until a correct Captcha is received (or 100 times, whichever comes first).
+For this, I first took a screenshot of the Captch element, and then process it using EasyOCR. The derived text is not always in the perfect form, thus after some tests, I found the characters are usually space separated, lowercase or in jumbled order. I added some processing to solve the space separated and lowercase problem. Once that is done, I input it in the field and check if it is correct.
+
+## Step 3 - Opening Links
+Using the find element path, I navigate through the different links. Once completed, it finally opens a new tab. From where we get the "Excel" button to download the relevant file.
+
+## Step 4 - Downloading Payment Details
+I click on the "Excel" button using the element_to_be_clicked function. Once clicked, it will start downloading, but as the downloading takes some time, I have added a sleep option. This will make sure the download is successfull. One can increase the sleep time according to the network.
+
+Note - The captcha screenshot is stored as captcha.png. We can use these images to train a model and increase the perfection instead of using EasyOCR.
--- main.py
+++ main.py
@@ -18,10 +18,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import io
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from io import BytesIO
 import pytesseract
 import easyocr
 import numpy as np
+
 
 
 import pandas as pd
@@ -38,7 +42,12 @@ def scrape_data(company_name: str):
     options.add_argument("--disable-extensions")
     options.add_argument("--ignore-certificate-errors")
     # TODO: Add whatever options you might think are helpful
-    prefs = {"download.default_directory" : os.path.join(os.getcwd(), DOWNLOAD_DIR)} # Set the download directory to the data folder
+    prefs = {
+    "download.default_directory": os.path.abspath(DOWNLOAD_DIR),
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "plugins.always_open_pdf_externally": True,
+    "safebrowsing.enabled": True}
     options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=options)
 
@@ -55,64 +64,74 @@ def scrape_data(company_name: str):
     # company_name = "MGH LOGISTICS PVT LTD"
     search_box = driver.find_element(By.ID, "estName")
     search_box.send_keys(company_name)
-    # _text = search_box.get_attribute("value")
-    # print(_text)
-    # print(company_name)
-
-    # Enter the captcha in the captcha box (Hint: You can use Google lens, Python OCR libraries like pytesseract, etc.)
-    captcha_element = driver.find_element(By.ID, "capImg")
-    driver.execute_script("arguments[0].scrollIntoView(true);", captcha_element)
-    time.sleep(1)
-    screenshot = driver.get_screenshot_as_png()
-
-    screenshot = Image.open(io.BytesIO(screenshot))
-    screenshot.save('screenshot.png')
-
-    location = captcha_element.location_once_scrolled_into_view
-    size = captcha_element.size
-
-    left = location['x'] + 610
-    top = location['y'] + 220
-    right = location['x'] + size['width'] + 720
-    bottom = location['y'] + size['height'] + 290
-
-    captcha_image = screenshot.crop((left, top, right, bottom))
-    captcha_image.save('captcha.png')
-    # captext = pytesseract.image_to_string(captcha_image)
-    captcha_image_np = np.array(captcha_image)
-
-    reader = easyocr.Reader(['en'])  # You can specify language(s) as needed
-    captcha_text = reader.readtext(captcha_image_np)
-
-    # Extract text from the OCR results
-    captcha_text = ' '.join([result[1] for result in captcha_text])
-
     
-    captcha_arr = captcha_text.split(' ')
+    for i in range(100):
+        try:
+            
+            # Enter the captcha in the captcha box (Hint: You can use Google lens, Python OCR libraries like pytesseract, etc.)
+            captcha_element = driver.find_element(By.ID, "capImg")
+            img_screenshot = captcha_element.screenshot_as_png
+            captcha_image = Image.open(BytesIO(img_screenshot))
+            captcha_image.save("captcha.png")
 
-    new_cap_txt = ""
-    for txt in captcha_arr:
-        new_cap_txt = new_cap_txt + txt
-    print(new_cap_txt)
-    captcha_box = driver.find_element(By.ID, "captcha")
-    captcha_box.send_keys(new_cap_txt)
+            ## PyTesseract
+            # image = cv2.imread('captcha.png')
+            # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+            # gray = cv2.medianBlur(gray, 3)
+            # filename = "{}.png".format("temp")
+            # cv2.imwrite(filename, gray)
+            # captcha_text = pytesseract.image_to_string(Image.open(filename))
 
+            ## EasyOCR
+            captcha_image_gray = captcha_image.convert('L')
+            captcha_image_np = np.array(captcha_image_gray)
+            reader = easyocr.Reader(['en'])  # You can specify language(s) as needed
+            captcha_text = reader.readtext(captcha_image_np)
+            captcha_text = ' '.join([result[1] for result in captcha_text])
+            captcha_text = captcha_text.upper()
+            print(captcha_text)
+            captcha_arr = captcha_text.split(' ')
+            new_cap_txt = ""
+            for txt in captcha_arr:
+                new_cap_txt = new_cap_txt + txt
 
-    # Click on the search button
-    search_button = driver.find_element(By.ID, "searchEmployer")
-    search_button.click()
-    time.sleep(5)
+            print(new_cap_txt)
+            captcha_box = driver.find_element(By.ID, "captcha")
+            captcha_box.send_keys(new_cap_txt)
+
+            search_button = driver.find_element(By.ID, "searchEmployer")
+            search_button.click()
+            time.sleep(2)
+            if "Please enter valid captcha" in driver.page_source:
+                print("Invalid captcha. Retrying...")
+                time.sleep(2)
+                continue
+            break
+        except:
+            print("Stale Element Reference Exception occurred. Retrying...")
 
     # TODO: Fill out the code for the following steps
     # Step 2 - Refer to the sample_output/step_2.png for the screenshot of the page
     # Click on the "View Details" button - you should see an output similar to sample_output/step_2.png
+    link = driver.find_element(By.XPATH, "//a[@title='Click to view establishment details.']")
+    link.click()
+    time.sleep(4)
     # Click on the "View Payment Details" button that is displayed in sample_output/step_2.png - this opens a new tab
+    link2 = driver.find_element(By.XPATH, "//a[@title='Click to view payment details.']")
+    link2.click()
+    time.sleep(4)
 
     # TODO: Fill out the code for the following steps
     # Step 3 - Refer to the sample_output/step_3.png for the screenshot of the page - this is the new tab that opens up in Step 2
     # Click on the "Excel" button - this downloads an excel file in the Downloads folder
+    driver.switch_to.window(driver.window_handles[-1])
+    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "dt-button.buttons-excel.buttons-html5")))
+    button.click()
     # This should be the final output of the function and should save the excel file in the data folder
     # It should look something like data/Payment Details.xlsx as used in the test_scrape_data() function
+    time.sleep(5)
+
 
 
 def test_scrape_data():
@@ -140,7 +159,7 @@ def main():
     scrape_data("MGH LOGISTICS PVT LTD")
 
     # TODO: Uncomment the following tests whenever scraping is completed.
-    # test_scrape_data()
+    test_scrape_data()
     # TODO: Feel free to add any edge cases which you might think are helpful
 
 
