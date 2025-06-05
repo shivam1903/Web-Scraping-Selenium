54f1690f02b6d8e113a9abb9e66921b5c7acf1a6
Shivam Agrawal <shivam234agarwal@gmail.com>
2025-06-05 22:19:02 +0530

    Removing extra comments

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-'''
-Use Python Selenium to scrape data from the EPFO website. Make sure to use the Chrome driver.
-Make sure to verify your requirements.txt file before submitting your code. You can use Python 3.11>= for this coding challenge.
-
-Fill out the sections where TODO is written.
-Ideally your code should work simply by running the main.py file.
-
-This is a sample file to get you started. Feel free to add any other functions, classes, etc. as you see fit.
-This coding challenge is designed to test your ability to write python code and your familiarity with the Selenium library.
-This coding challenge is designed to take 2-4 hours and is representative of the kind of work you will be doing at the company daily.
-'''
 
 # Importing the required libraries
 import os
@@ -58,17 +47,13 @@ def scrape_data(company_name: str):
     import time
     time.sleep(5)
 
-    # TODO: Fill out the code for the following steps
-    # Step 1 - Refer to the sample_output/step_1.png for the screenshot of the page
     # Enter the company name in the search box
-    # company_name = "MGH LOGISTICS PVT LTD"
     search_box = driver.find_element(By.ID, "estName")
     search_box.send_keys(company_name)
     
     for i in range(100):
         try:
             
-            # Enter the captcha in the captcha box (Hint: You can use Google lens, Python OCR libraries like pytesseract, etc.)
             captcha_element = driver.find_element(By.ID, "capImg")
             img_screenshot = captcha_element.screenshot_as_png
             captcha_image = Image.open(BytesIO(img_screenshot))
@@ -111,25 +96,18 @@ def scrape_data(company_name: str):
         except:
             print("Stale Element Reference Exception occurred. Retrying...")
 
-    # TODO: Fill out the code for the following steps
-    # Step 2 - Refer to the sample_output/step_2.png for the screenshot of the page
-    # Click on the "View Details" button - you should see an output similar to sample_output/step_2.png
+    
     link = driver.find_element(By.XPATH, "//a[@title='Click to view establishment details.']")
     link.click()
     time.sleep(4)
-    # Click on the "View Payment Details" button that is displayed in sample_output/step_2.png - this opens a new tab
     link2 = driver.find_element(By.XPATH, "//a[@title='Click to view payment details.']")
     link2.click()
     time.sleep(4)
 
-    # TODO: Fill out the code for the following steps
-    # Step 3 - Refer to the sample_output/step_3.png for the screenshot of the page - this is the new tab that opens up in Step 2
-    # Click on the "Excel" button - this downloads an excel file in the Downloads folder
     driver.switch_to.window(driver.window_handles[-1])
     button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "dt-button.buttons-excel.buttons-html5")))
     button.click()
-    # This should be the final output of the function and should save the excel file in the data folder
-    # It should look something like data/Payment Details.xlsx as used in the test_scrape_data() function
+
     time.sleep(5)
 
 
@@ -158,9 +136,7 @@ def main():
 
     scrape_data("MGH LOGISTICS PVT LTD")
 
-    # TODO: Uncomment the following tests whenever scraping is completed.
     test_scrape_data()
-    # TODO: Feel free to add any edge cases which you might think are helpful
 
 
 if __name__ == "__main__":
